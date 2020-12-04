41704e559b1f80265a76c96501fb65553bcabe44
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-03 19:58:24 -0500

    devrait nous retourner juste le html
    03-12-2020

--- CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
+++ CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
@@ -7,7 +7,13 @@ class MaisonsSpider(scrapy.Spider):
         "https://duproprio.com/fr/quebec-rive-sud/levis/maison-a-vendre/hab-5338-rue-saint-georges-744976#la-capitale"]
         
     def parse(self, response):
+        page = response.url
+        filename = "essai_page_web1.html"
+        with open(filename, "wb") as f:
+            f.write(response.body)
+        
+    '''def parse(self, response):
         page = response.url.split('/')[-1] #Juste obtenir la page, on peut parser comme on veut
         filename = "post-%s.html" % page
         with open(filename, 'wb') as f:
-            f.write(response.body)
+            f.write(response.body)'''
