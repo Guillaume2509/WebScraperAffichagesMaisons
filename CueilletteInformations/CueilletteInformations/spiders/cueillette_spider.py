e1ccdb0fae4eacbe244103f3f7aa438206319b2f
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-06 18:38:17 -0500

    Première adresse sauvegardée

--- CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
+++ CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
@@ -6,12 +6,17 @@ class MaisonsSpider(scrapy.Spider):
     start_urls = [
         "https://duproprio.com/fr/quebec-rive-sud/levis/maison-a-vendre/hab-5338-rue-saint-georges-744976#la-capitale"]
         
-    def parse(self, response):
+    '''def parse(self, response):
         page = response.url
         filename = "essai_page_web1.html"
         with open(filename, "wb") as f:
-            f.write(response.body)
+            f.write(response.body)'''
 
     #parse2
-    '''def parse(self, response):
-        for post in response.xpath("blablabla"):'''
+    def parse(self, response):
+        adresse = response.xpath('/html/body/main/div[1]/div/div[2]/div[1]/div[1]/div[1]/div[3]/div/h1/text()').extract()
+        filename = str(adresse) + '.txt'
+        #filename = "essai_page_web1.txt"
+        print(adresse)
+        with open(filename, "w") as f:
+            f.write(str(adresse))
