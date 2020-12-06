900b0128642378d31be26008ec1b9096fcac7544
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-05 22:04:01 -0500

    added
    05-12-2020

--- CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
+++ CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
@@ -12,8 +12,3 @@ class MaisonsSpider(scrapy.Spider):
         with open(filename, "wb") as f:
             f.write(response.body)
         
-    '''def parse(self, response):
-        page = response.url.split('/')[-1] #Juste obtenir la page, on peut parser comme on veut
-        filename = "post-%s.html" % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)'''
