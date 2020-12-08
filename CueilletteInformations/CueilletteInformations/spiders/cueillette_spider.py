48443ad9653b533774f8daec87066b6a63a00b48
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-07 20:45:00 -0500

    updated
    07-12-2020

--- CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
+++ CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
@@ -6,11 +6,6 @@ class MaisonsSpider(scrapy.Spider):
     start_urls = [
         "https://duproprio.com/fr/quebec-rive-sud/levis/maison-a-vendre/hab-5338-rue-saint-georges-744976#la-capitale"]
         
-    '''def parse(self, response):
-        page = response.url
-        filename = "essai_page_web1.html"
-        with open(filename, "wb") as f:
-            f.write(response.body)'''
 
     #parse2
     def parse(self, response):
