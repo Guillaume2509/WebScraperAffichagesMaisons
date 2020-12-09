1c33ec1fd7210946572eea8c7282eb8643e6c991
Guillaume Giroux <45839232+Guillaume2509@users.noreply.github.com>
2020-12-08 22:44:15 -0500

    adresse, prix, surface
    08-12-2020

--- CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
+++ CueilletteInformations/CueilletteInformations/spiders/cueillette_spider.py
@@ -1,17 +1,29 @@
 import scrapy
 
 class MaisonsSpider(scrapy.Spider):
+    
+    #Nom du spider
     name = "AffichageMaisons"
     
+    #Liste des URL à Scraper
     start_urls = [
         "https://duproprio.com/fr/quebec-rive-sud/levis/maison-a-vendre/hab-5338-rue-saint-georges-744976#la-capitale"]
         
 
-    #parse2
+    #Parser
     def parse(self, response):
+        
+        #Nom du fichier texte
+        filename = 'maisons.txt'
+        
+        #Extraction de l'adresse, prix et surface
         adresse = response.xpath('/html/body/main/div[1]/div/div[2]/div[1]/div[1]/div[1]/div[3]/div/h1/text()').extract()
-        filename = str(adresse) + '.txt'
-        #filename = "essai_page_web1.txt"
-        print(adresse)
+        prix = response.xpath('/html/body/main/div[1]/div/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/text()').extract()
+        surface = response.xpath('/html/body/main/div[1]/div/div[1]/section[1]/article/div[1]/div[4]/div[1]/div[2]/span[2]/text()').extract()
+        
+        #Création d'une ligne type csv
+        line = str(adresse) + "," + str(prix) + "," + str(surface)
+
+        #Ajout de la ligne au fichier
         with open(filename, "w") as f:
-            f.write(str(adresse))
+            f.write(line)
