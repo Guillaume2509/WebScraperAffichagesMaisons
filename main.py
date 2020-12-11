a28fb9cf0ed608e574a726576add9df62485ec93
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-10 22:00:31 -0500

    main
    10-12-2020

--- main.py
+++ main.py
@@ -2,22 +2,6 @@ from scrapy.crawler import CrawlerProcess
 from CueilletteInformations.CueilletteInformations.spiders.cueillette_spider import MaisonsSpider
 
 #TODO: créer le sous-projet et réajuster l'architecture du code: https://youtu.be/Wp6LRijW9wg
- #a
-#spider
-
-class MaisonsSpider(scrapy.Spider):
-    name = "AffichageMaisons"
-    
-    start_urls = [ #à corriger, de cierto
-        "https://duproprio.com/page/1"
-        "https://remax.com"
-        "https://royallepage.com"]
-        
-    def parse(self, response):
-        page = response.url.split('/')[-1] #Juste obtenir la page, on peut parser comme on veut
-        filename = "post-%s.html" % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
             
 
 if __name__ == "__main__":
