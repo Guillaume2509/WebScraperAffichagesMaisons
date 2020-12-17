59adf054e5e61fae4c91ddac6ad25d7b4b781c23
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-16 21:46:56 -0500

    nouveau dossier simplifier à essayer
    16-12-2020

---
+++ CueilletteInformations-2/spiders/cueillette_spider.py
@@ -0,0 +1,40 @@
+import scrapy
+import re
+
+class MaisonsSpider(scrapy.Spider):
+    
+    #Nom du spider
+    name = "AffichageMaisons"
+    
+    #Liste des URL à Scraper
+    start_urls = [
+        "https://duproprio.com/fr/quebec-rive-sud/levis/maison-a-vendre/hab-5338-rue-saint-georges-744976#la-capitale"]
+        
+
+    #Parser
+    def parse(self, response):
+        
+        #Nom du fichier texte
+        filename = 'maisons.txt'
+        
+        #Extraction de l'adresse, prix et surface
+        adresse = response.xpath('/html/body/main/div[1]/div/div[2]/div[1]/div[1]/div[1]/div[3]/div/h1/text()').extract()
+        adresse = str(adresse)
+        adresse = adresse.replace('[\'', '').replace(',', '').replace('\']', '')
+
+        prix = response.xpath('/html/body/main/div[1]/div/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/text()').extract()
+        prix = str(prix)
+        prix = prix.replace('\\xa0', '')
+        prix = re.search("\\d+\\$", prix, re.MULTILINE).group()
+
+        surface = response.xpath('/html/body/main/div[1]/div/div[1]/section[1]/article/div[1]/div[4]/div[1]/div[2]/span[2]/text()').extract()
+        surface = str(surface)
+        surface = re.search("\\d*\\s?\\d*\\s?\\d+\\spi", surface, re.MULTILINE).group()
+        surface = surface.replace(' ', '') + "2"
+        
+        #Création d'une ligne type csv
+        line = adresse + "," + prix + "," + surface
+
+        #Ajout de la ligne au fichier
+        with open(filename, "w") as f:
+            f.write(line)
