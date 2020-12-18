6361d69a05b155d9bb48505c90e49b35c9b88885
Guillaume Giroux <guillaume.giroux@live.ca>
2020-12-17 20:08:55 -0500

    préparation processing
    17-12-2020

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from scrapy.crawler import CrawlerProcess
 from CueilletteInformations.CueilletteInformations.spiders.cueillette_spider import MaisonsSpider
 
-#TODO: créer le sous-projet et réajuster l'architecture du code: https://youtu.be/Wp6LRijW9wg
+
+maisons_path = "path.csv"
             
 
 if __name__ == "__main__":
     process = CrawlerProcess()
 
 process.crawl(MaisonsSpider)
+
+    maisons = pd.read_csv(maisons_path)
+    
