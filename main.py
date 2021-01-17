a74d9e3e960cf00b73012b404fffc1484458f13a
Guillaume Giroux <guillaume.giroux@live.ca>
2021-01-17 12:19:42 -0500

    Placé et décrit le crawler débuté à même le Script
    17-01-2021

--- main.py
+++ main.py
@@ -7,9 +7,16 @@ from Scraping.Scraping.spiders.cueillette_spider import MaisonsSpider
 def Scraping():
     '''Appeler le Scraping'''
     #Tutoriel: https://towardsdatascience.com/how-to-run-scrapy-from-a-script-ff07fd6b792b
+    
+    #Ici on veut starter le crawler à même le script python, ainsi on fait:
+    #1) On importe l'objet CrawlerProcess(), qui englobe le module Twister de Python
     process = CrawlerProcess()
     
+    #2) On configure l'objet CrawlerProcess à notre spider MaisonSpider
     process = process.crawl(MaisonsSpider)
+    
+    #3) On démarre le Crawler, comme si on était sur la ligne de commande. Il devrait nous retourner notre fichier CSV au Scraping/maisons.csv
+    process.start()
     
 def Scoring():
     '''Construire le Scoring'''
