e88d25efb1832ea45354fe2eb560c8eba43e6111
Guillaume Giroux <guillaume.giroux@live.ca>
2021-01-16 23:10:03 -0500

    edited the main
    16-01-2021

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Scraping.Scraping.spiders.cueillette_spider import MaisonsSpider
 
 def Scraping():
     '''Appeler le Scraping'''
+    #Tutoriel: https://towardsdatascience.com/how-to-run-scrapy-from-a-script-ff07fd6b792b
     process = CrawlerProcess()
     
     process = process.crawl(MaisonsSpider)
@@ -16,7 +17,11 @@ def Scoring():
     
     with open('Scraping/maisons.csv', 'wb') as f:
 
-    return "Todo"
+    return todo
+    
+def display():
+    '''Print the results'''
+    read_csv(mettre le path du csv ici)
             
 
 if __name__ == "__main__":
